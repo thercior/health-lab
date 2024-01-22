4f5020ed43776597c9ce30e24533ee4c1e9384cc
Thércio Rodrigues <thercior@gmail.com>
2024-01-22 12:01:07 -0300

    Create application

---
+++ apps/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from users.views import *
+
+app_name = 'Users'
+
+urlpatterns = [
+    path('cadastro/', cadastro, name='cadastro_user')
+]
---
+++ apps/users/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'Users/cadastro.html')
+    
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        if not password == confirm_password:
+            return redirect('Users:cadastro_user')
+        
+        if len(password) < 6:
+            return redirect('Users:cadastro_user')
+        
+        try:
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+            )
+        except:
+            return redirect('Users:cadastro_user')
+    
+    return redirect('Users:cadastro_user')
