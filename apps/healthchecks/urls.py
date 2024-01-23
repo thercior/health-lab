4113aa8d9aaff045af5b82e0a07b768c6b69197f
Thércio Rodrigues <thercior@gmail.com>
2024-01-22 21:53:47 -0300

    Create template e rotas de healthchecks

---
+++ apps/healthchecks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from healthchecks.views import *
+
+app_name = 'HealthChecks'
+
+urlpatterns = [
+    path('solicitar_exames/', request_exams, name='requesst_exam'),
+    path('checkout_pedido/', close_order, name='close_order')
+]
