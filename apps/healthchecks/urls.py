d8dbdbc95d37a36a26ce7314ca0152cacfea753b
Thércio Rodrigues <thercior@gmail.com>
2024-01-22 21:58:28 -0300

    Fix name route 'request_exams'

--- apps/healthchecks/urls.py
+++ apps/healthchecks/urls.py
@@ -4,6 +4,6 @@ from healthchecks.views import *
 app_name = 'HealthChecks'
 
 urlpatterns = [
-    path('solicitar_exames/', request_exams, name='requesst_exam'),
+    path('solicitar_exames/', request_exams, name='request_exam'),
     path('checkout_pedido/', close_order, name='close_order')
 ]
