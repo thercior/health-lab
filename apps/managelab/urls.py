7fe7137dba715d0d021f052dab7a030d5ffd9c80
Thércio Rodrigues <thercior@gmail.com>
2024-01-25 11:51:58 -0300

    Criação da rota e da view para alterar informações do exame e add url da rota no template implementando parâmetro enctype

--- apps/managelab/urls.py
+++ apps/managelab/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('clientes/<int:client_id>', client, name='client'),
     path('exame_cliente/<int:exam_id>', exam_client, name='exam_client'),
     path('proxy_pdf/<int:exam_id>', proxy_pdf, name='proxy_pdf'),
-    path('gerar_senha/<int:exam_id>', generate_pass, name='generate_pass')
+    path('gerar_senha/<int:exam_id>', generate_pass, name='generate_pass'),
+    path('alterar_dados_exame/<int:exam_id>', alter_data_exam, name='alter_data_exam'),
 ]
--- apps/managelab/views.py
+++ apps/managelab/views.py
@@ -1,9 +1,12 @@
+from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
 from django.db.models import Value
 from django.db.models.functions import Concat
 from django.http import FileResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from healthchecks.models import RequestHealthChecks
 from managelab.utils import pdf_pass_exams, random_pass_generate
 
@@ -74,4 +77,24 @@ def generate_pass(request, exam_id):
     exam.save()
     
     return FileResponse(pdf_pass_exams(exam.exam.name, exam.user, exam.password), filename='token.pdf')
+
+def alter_data_exam(request, exam_id):
+    exam = RequestHealthChecks.objects.get(id=exam_id)
+    pdf = request.FILES.get('result')
+    status = request.POST.get('status')
+    required_password = request.POST.get('required_password')
     
+    if required_password and (not exam.password):
+        messages.add_message(request, constants.WARNING, 'Para exigir senha é necessário gerar uma primeiro')
+        return redirect(reverse('ManageLab:exam_client', kwargs={'exam_id': exam_id}))
+    
+    exam.required_password = True if required_password else False
+    
+    if pdf:
+        exam.result = pdf
+    
+    exam.status = status
+    exam.save()
+    messages.add_message(request, constants.SUCCESS, 'Alteração realizada com sucesso')
+    
+    return redirect(reverse('ManageLab:exam_client', kwargs={'exam_id': exam_id}))
